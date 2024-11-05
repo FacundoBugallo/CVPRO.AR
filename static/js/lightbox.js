c7e2955f0bac7d05ad554f477b16deed077347b2
Facundo Bugallo <facundobugallo881@gmail.com>
2024-11-04 22:29:29 -0300

    fix( lectura incorrecta de el src de las imagenes en el lightbox)

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,35 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Seleccionamos todas las imágenes que queremos que abran en pantalla grande
   const images = document.querySelectorAll('.cv-item');
   const lightbox = document.createElement('div');
   lightbox.classList.add('lightbox');
   
-  // Creamos la imagen en pantalla grande
   const img = document.createElement('img');
   lightbox.appendChild(img);
   
-  // Botón de cerrar
   const closeButton = document.createElement('button');
-  closeButton.innerHTML = '&times;'; // Icono de cerrar
+  closeButton.innerHTML = '&times;';
   closeButton.classList.add('lightbox-close');
   lightbox.appendChild(closeButton);
   
   document.body.appendChild(lightbox);
-  
+
   images.forEach(image => {
     image.addEventListener('click', () => {
-      img.src = image.getAttribute('data-image'); // Asignamos la imagen al lightbox
-      lightbox.style.display = 'flex'; // Mostramos el lightbox
+      const imageUrl = image.querySelector('img').getAttribute('data-image');
+      console.log("URL de la imagen:", imageUrl); // Depuración
+      if (imageUrl) {
+        img.src = imageUrl;
+        lightbox.style.display = 'flex';
+      } else {
+        console.error("No se encontró el atributo 'data-image' en la imagen seleccionada.");
+      }
     });
   });
 
-  // Cerrar el lightbox al hacer clic en el botón de cerrar o en el fondo
   closeButton.addEventListener('click', () => {
     lightbox.style.display = 'none';
   });
 
   lightbox.addEventListener('click', (e) => {
-    if (e.target === lightbox) { // Solo cerramos si se hace clic fuera de la imagen
+    if (e.target === lightbox) {
       lightbox.style.display = 'none';
     }
   });
